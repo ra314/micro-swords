c7673052609923656ee61b8bdfbe6280c19926e3
ra314 <rahulgandharva20122000@gmail.com>
2023-07-17 18:33:32 +1000

    Implemented the reset_to_default button.

--- Scripts/Settings.gd
+++ Scripts/Settings.gd
@@ -11,7 +11,21 @@ func _ready():
 	$VBox/DOUBLE_JUMP.button_up.connect(func(): ConstData.DOUBLE_JUMP = !ConstData.DOUBLE_JUMP)
 	$VBox/VARIABLE_JUMP_HEIGHT.button_up.connect(func(): ConstData.VARIABLE_JUMP_HEIGHT = !ConstData.VARIABLE_JUMP_HEIGHT)
 	$VBox/SEPARATE_BUTTONS.button_up.connect(func(): ConstData.SEPARATE_BUTTONS = !ConstData.SEPARATE_BUTTONS)
+	$Reset.button_up.connect(reset_to_default)
 	root = get_parent().get_parent()
+
+func reset_to_default():
+	for child in $VBox.get_children():
+		if child is HBoxContainer:
+			var property_name = child.name
+			var spinbox = child.get_node("Label2")
+			spinbox.value = ConstData.DEFAULTS[property_name][2]
+	ConstData.DOUBLE_JUMP = false
+	ConstData.VARIABLE_JUMP_HEIGHT = false
+	ConstData.SEPARATE_BUTTONS = false
+	$VBox/VARIABLE_JUMP_HEIGHT.button_pressed = false
+	$VBox/SEPARATE_BUTTONS.button_pressed = false
+	$VBox/DOUBLE_JUMP.button_pressed = false
 
 func init_check_boxes():
 	$VBox/VARIABLE_JUMP_HEIGHT.button_pressed = ConstData.VARIABLE_JUMP_HEIGHT
@@ -24,13 +38,13 @@ func init_text():
 			var property_name = child.name
 			var spinbox = child.get_node("Label2")
 			var extents = child.get_node("Label3")
-			spinbox.value = ConstData.DEFAULTS[property_name][2]
 			var max = ConstData.DEFAULTS[property_name][1]
 			var min = ConstData.DEFAULTS[property_name][0]
 			extents.text = "min: %d, max: %d" % [min, max]
 			spinbox.min_value = min
 			spinbox.max_value = max
 			spinbox.step = ConstData.DEFAULTS[property_name][3]
+			spinbox.value = ConstData.DEFAULTS[property_name][2]
 
 func load_level_selector():
 	var level_selector = load("res://Scenes/Level Selector.tscn").instantiate()
